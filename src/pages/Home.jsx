a2ecdf3c98b62068bacbeef548b21ec98dfdf563
Samyu <samyuktha.ara@gmail.com>
2026-01-14 16:45:58 +0530

    1401

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Hero from "../components/Home/Hero";
 import BodyContouringHero from "../components/Home/HomeHero";
 import ServicesSection from "../components/Home/ServicesSection";
@@ -11,20 +14,48 @@ import PatientTestimonials from "../components/Home/PatientTestimonials";
 import FAQ from "../components/Home/FAQ";
 import TransformationSteps from "../components/Home/TransformationSteps";
 
-
 const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, // animation duration
+      offset: 80, // how far from viewport
+      once: true, // whether animation happens only once
+      easing: "ease-in-out",
+    });
+  }, []);
+
   return (
     <div id="home">
-      <BodyContouringHero />
-      <ServicesSection />
-      <HeroVideo />
-      <BookAppointment />
-      <AreasOfExpertise />
-      <BodyTreatment />
-      <HolisticApproach />
-      <PatientTestimonials />
-      <FAQ />
-      <TransformationSteps/>
+      <div data-aos="fade-up">
+        <BodyContouringHero />
+      </div>
+      <div data-aos="fade-up">
+        <ServicesSection />
+      </div>
+      <div data-aos="fade-down">
+        <HeroVideo />
+      </div>
+      <div data-aos="fade-up">
+        <BookAppointment />
+      </div>
+      <div data-aos="fade-left">
+        <AreasOfExpertise />
+      </div>
+      <div data-aos="fade-right">
+        <BodyTreatment />
+      </div>
+      <div data-aos="fade-up">
+        <HolisticApproach />
+      </div>
+      <div data-aos="zoom-in">
+        <PatientTestimonials />
+      </div>
+      <div data-aos="fade-up">
+        <FAQ />
+      </div>
+      <div data-aos="fade-up">
+        <TransformationSteps />
+      </div>
     </div>
   );
 };
--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import ClientSuccessHero from "../components/Testimonials/ClientSuccessHero";
 import ClientJourneys from "../components/Testimonials/ClientJourneys";
-
 import ClientTestimonials from "../components/Testimonials/Testimonials";
 import JourneySection from "../components/Testimonials/JourneySection";
 
 const Testimonial = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1100,
+      once: true,
+      easing: "ease-in-out",
+    });
+  }, []);
+
   return (
     <div id="testimonial">
-      <ClientSuccessHero />
-      <ClientJourneys />
-      <ClientTestimonials />
-      <JourneySection />
+      <div data-aos="fade-up">
+        <ClientSuccessHero />
+      </div>
+      <div data-aos="fade-right" data-aos-delay="200">
+        <ClientJourneys />
+      </div>
+      <div data-aos="zoom-in" data-aos-delay="300">
+        <ClientTestimonials />
+      </div>
+      <div data-aos="fade-up" data-aos-delay="400">
+        <JourneySection />
+      </div>
     </div>
   );
 };
